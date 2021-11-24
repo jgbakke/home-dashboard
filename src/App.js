5acd446b7ea82c8969dab8578cb46b7168ca93a9
Jordan Bakke <redwingjordan@gmail.com>
2021-11-23 23:19:55 -0600

    More UI experimenting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import './proxima-nova.css'
 import AppSelector from './AppSelector.js'
 
 function App() {
-  return (
-    <div id="selector-panel">
-        <AppSelector name="Market Update" />
-        <AppSelector name="Game Sales" />
-        <AppSelector name="NHL Scores" />
-        <AppSelector name="COVID Stats" />
-        <AppSelector name="Programming News" />
-    </div>
+    return (
+        <>
+            <div id="selector-panel">
+                <AppSelector name="Market Update" />
+                <AppSelector name="Game Sales" />
+                <AppSelector name="NHL Scores" />
+                <AppSelector name="COVID Stats" />
+                <AppSelector name="Programming News" />
+            </div>
+            <div id="app-body">
+                <p class="tk-proxima-nova">Please select an app to use.</p>
+            </div>
+        </>
   );
 }
 
